351728bcd171e0d96ffaf687f38e572a726e6a57
cains <cainsteven57@gmail.com>
2025-03-01 12:40:36 -0600

    Added __init__ and set up desk and cart size

--- PagingMemoryManagementExample/PagingMemoryManagementExample.py
+++ PagingMemoryManagementExample/PagingMemoryManagementExample.py
@@ -1,4 +1,13 @@
 
+class paging_system:
+    
+    def __init__ (self, desk_size, cart_size ):
+        self.desk = [] # the desk is what would be in your ram
+        self.cart = [] # the cart is what would be in your Vram 
+        self.library = ["Dinosour Dogs", " The Early History of Dogs", "Dogs in Mythology",
+                            "Ancient Civilizations with Dogs", "Famous Dogs in Pop Culture", "Dog Breeds And Traits",
+                            "Role of Dogs in Companionship"] #library would be everything in your HHD 
 
-if __name__ == '__main__':
-    print('hello')
+        self.cart_size = cart_size # cart size is how big your Vram is 
+        self.desk_size = 3 # Desk size is how big your RAM is 
+            
