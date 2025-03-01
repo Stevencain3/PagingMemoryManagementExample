a349dcaa93cff040e600cd55dd9c3c2a720dd6fc
cains <cainsteven57@gmail.com>
2025-03-01 12:55:28 -0600

    Finished request_book function will need to finish move_to_desk and get_from_shelfs to properly work

--- PagingMemoryManagementExample/PagingMemoryManagementExample.py
+++ PagingMemoryManagementExample/PagingMemoryManagementExample.py
@@ -6,8 +6,20 @@ class paging_system:
         self.cart = [] # the cart is what would be in your Vram 
         self.library = ["Dinosour Dogs", " The Early History of Dogs", "Dogs in Mythology",
                             "Ancient Civilizations with Dogs", "Famous Dogs in Pop Culture", "Dog Breeds And Traits",
-                            "Role of Dogs in Companionship"] #library would be everything in your HHD 
+                            "Role of Dogs in Companionship"] #library would be everything in your HDD 
 
         self.cart_size = cart_size # cart size is how big your Vram is 
         self.desk_size = 3 # Desk size is how big your RAM is 
             
+        #this would be like doing a paging request 
+    def request_book(self, book):
+        if book in desk:
+           print ("{book} is on the desk(RAM) already!")
+        elif book in cart:
+            print ("moving {book} from the cart(VRAM) to the desk(RAM)")
+            self.move_to_desk(book)
+        elif book in library:
+            print("{book} is on the libray shelves(HDD) im fetiching it now.")
+            self.get_from_shelves(book)
+        else: 
+            print("{book} is not offered here in the library")
